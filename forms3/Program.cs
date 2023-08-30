7d586a22d5a63cf4de086218bd5e5719b2f180cf
muril0ve <126685404+muril0ve@users.noreply.github.com>
2023-08-30 10:33:03 -0300

    Program.cs

--- forms3/Program.cs
+++ forms3/Program.cs
@@ -1,22 +1,80 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace forms3
 {
-    internal static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string enrollment = textBox2.Text;
+
+            string message = "Login: " + name +
+                             "\nSenha: " + enrollment;
+            MessageBox.Show(
+                message,
+                "Atenção",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+        
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+       
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            
+            
         }
     }
 }
